9e2116932ab1b48c7e55ea62a12954eef248cde8
David Alexander Pfeiffer <david.pfeiffer971@gmail.com>
2023-01-16 15:21:31 +0100

    client vite application scaffold

---
+++ client/src/App.tsx
@@ -0,0 +1,50 @@
+import { FormEventHandler, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [messageList, setMessageList] = useState([]);
+  const [nickName, setNickName] = useState("");
+  const [newMessageText, setNewMessageText] = useState("");
+  const [socket, setSocket] = useState(null);
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="App">
+      <h2>Messages</h2>
+      {messageList.map((message) => {
+        return (
+          <div key={message.id}>
+            {message.author} : {message.text}
+          </div>
+        );
+      })}
+
+      <form onSubmit={handleSubmit}>
+        <h2>New Message</h2>
+        <input
+          type="text"
+          name="author"
+          placeholder="nickname"
+          value={nickName}
+          required
+          onChange={(e) => setNickName(e.target.value)}
+        />
+        <input
+          type="text"
+          name="messageContent"
+          placeholder="message"
+          value={newMessageText}
+          required
+          onChange={(e) => setNewMessageText(e.target.value)}
+        />
+        <input type="submit" value="send" />
+      </form>
+    </div>
+  );
+}
+
+export default App;
