1b4ea5b978de7559aef0793ce91875a6a420706c
David Alexander Pfeiffer <david.pfeiffer971@gmail.com>
2023-01-16 17:38:26 +0100

    added import for crypto

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import crypto from "crypto";
 import { createServer } from "http";
 import { Server as WebSocketServer } from "socket.io";
 
